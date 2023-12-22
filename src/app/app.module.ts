84e44b17067c68ed31e8de803ece6c635ea0e9f2
Szebastian <sebastianmarcoscontreras@gmail.com>
2023-12-22 09:55:42 -0300

    inicio de proyecto para los pipesApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+
+//configuracion del local de la app
+import localeEsAR from '@angular/common/locales/es-AR';
+import localeFrCa from '@angular/common/locales/fr-CA';
+
+
+import { registerLocaleData } from '@angular/common';
+
+
+
+registerLocaleData(localeEsAR);
+registerLocaleData(localeFrCa);
+
 
 @NgModule({
   declarations: [
@@ -10,11 +25,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' },
     provideClientHydration()
   ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
